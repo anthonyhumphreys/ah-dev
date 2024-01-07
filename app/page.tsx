70a79940759a5f398112cf8750aa82228a4c3a6f
Anthony Humphreys <anthony@lexio.app>
2024-01-07 13:56:26 +0000

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Footer } from '@/components/Footer/Footer';
 
 export default function HomePage() {
   return <Welcome />;
