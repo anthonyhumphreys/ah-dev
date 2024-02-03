a699b6748e3a784c46f60333d95c93a4d1f95319
Anthony Humphreys <anthony@lexio.app>
2024-02-03 17:34:51 +0000

    disable api route

--- app/projects/ai-scrum-assistant/user-stories/page.tsx
+++ app/projects/ai-scrum-assistant/user-stories/page.tsx
@@ -1,10 +1,25 @@
+'use client';
+
 import { Container } from '@mantine/core';
+import { useChat } from 'ai/react';
+import Chat from '@/components/Chat/Chat';
 
 export default function UserStories() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    initialInput: '',
+  });
+
+  const createdUserStories = messages.map((message, i) => (
+    <div key={i}>
+      <p>{message}</p>
+    </div>
+  ));
+
   return (
     <Container size="md">
       <h1>User Stories</h1>
-      {/* <Chat /> */}
+      {/* @ts-ignore */}
+      <Chat handleSubmit={handleSubmit} input={input} handleInputChange={handleInputChange} />
     </Container>
   );
 }
--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,26 +1,39 @@
 'use client';
 
-import { useChat } from 'ai/react';
+import { ActionIcon, TextInput, rem, useMantineTheme } from '@mantine/core';
+import { IconArrowRight, IconSearch } from '@tabler/icons-react';
+import { ChatRequestOptions } from 'ai';
+import { FormEvent } from 'react';
 
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+type Props = {
+  handleSubmit: () => void;
+  input: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Chat({ handleSubmit, input, handleInputChange }: Props) {
+  const theme = useMantineTheme();
+
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map((m) => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
+    <TextInput
+      radius="xl"
+      size="md"
+      placeholder="Say something..."
+      rightSectionWidth={42}
+      leftSection={<IconSearch style={{ width: rem(18), height: rem(18) }} stroke={1.5} />}
+      rightSection={
+        <ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
+          <IconArrowRight style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+        </ActionIcon>
+      }
+      onChange={handleInputChange}
+      value={input}
+      onKeyDown={(event) => {
+        if (event.key === 'Enter') {
+          // @ts-ignore
+          handleSubmit(event);
+        }
+      }}
+    />
   );
 }
