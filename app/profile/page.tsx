42b1e82d84a966cf8677f5ef5f1b8c8dd9a44efc
Anthony Humphreys <contact@anthonyhumphreys.co.uk>
2024-08-30 20:29:17 +0100

    fix ProfileServer component return type

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,14 +6,14 @@ export default withPageAuthRequired(
 
     const user = session?.user;
 
-    return (
-      user && (
-        <div>
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </div>
-      )
+    return user ? (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    ) : (
+      <p>No user found</p>
     );
   },
   { returnTo: '/profile' }
